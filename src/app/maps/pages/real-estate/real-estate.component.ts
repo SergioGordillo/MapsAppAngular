8d4c1a7b6c40ac5c633913352469d5af4a601db3
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-10-12 23:29:55 +0200

    Creating structure of the project

---
+++ src/app/maps/pages/real-estate/real-estate.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-real-estate',
+  templateUrl: './real-estate.component.html',
+  styles: [
+  ]
+})
+export class RealEstateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
