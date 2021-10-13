f8d1c13546bd3cab9a6baed1a45d8ccb6b19c8ca
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-10-13 19:09:34 +0200

    Structure of application and PoC of menu created

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
-  styleUrls: ['./mini-map.component.css']
 })
 export class MiniMapComponent implements OnInit {
 
--- src/app/maps/maps-routing.module.ts
+++ src/app/maps/maps-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullScreenComponent } from './pages/full-screen/full-screen.component';
+import { ZoomRangeComponent } from './pages/zoom-range/zoom-range.component';
+import { MarkersComponent } from './pages/markers/markers.component';
+import { RealEstateComponent } from './pages/real-estate/real-estate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {path: 'fullscreen', component: FullScreenComponent},
+      {path: 'zoom-range', component: ZoomRangeComponent},
+      {path: 'markers', component: MarkersComponent},
+      {path: 'real-estate', component: RealEstateComponent},
+      {path: '**', redirectTo: 'fullscreen'},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
