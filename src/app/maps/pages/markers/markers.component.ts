7b0b93d9d71f659d3b68d9e71dc7426ed8935b3b
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-10-19 17:37:00 +0200

    feat: zoom-range done and working on markers

--- src/app/maps/pages/markers/markers.component.ts
+++ src/app/maps/pages/markers/markers.component.ts
@@ -1,16 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-markers',
   templateUrl: './markers.component.html',
   styles: [
+    `
+    .map-container{
+      width: 100%;
+      height: 100%
+    }
+
+    #zoom-level {
+      background-color: white;
+      position: fixed;
+      bottom: 50px;
+      left: 50px;
+      padding: 10px;
+      border-radius: 5px;
+      z-index: 999;
+      width: 400px;
+    }
+
+    #add-marker{
+      position: fixed; 
+      bottom: 50px;
+      left: 40px;
+      width: 400px;
+      height: 200px;
+    }
+  `
   ]
 })
-export class MarkersComponent implements OnInit {
+export class MarkersComponent implements AfterViewInit{
+
+  @ViewChild('map') divMap!: ElementRef; 
+  map!:mapboxgl.Map; 
+  zoomLevel: number = 15; 
+  mapCenter: [number, number] = [13.378491055638413, 52.51636916493755]
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    this.map.off('zoom', ()=>{})
+    this.map.off('zoomend', ()=>{})
+    this.map.off('move', ()=>{})
   }
 
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+    container: this.divMap.nativeElement,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: this.mapCenter,
+    zoom: this.zoomLevel
+  });
+
+  this.map.on('zoom', ()=> {
+    this.zoomLevel=this.map.getZoom(); 
+  });
+
+  this.map.on('zoomend', ()=> {
+    if (this.map.getZoom()>18){
+      this.map.zoomTo(18);
+    }
+  })
+
+  this.map.on('move', (event)=> {
+    const target = event.target;
+    const {lng, lat}=target.getCenter(); 
+    this.mapCenter=[lng, lat];
+  });
+
+  const marker=new mapboxgl.Marker()
+    .setLngLat(this.mapCenter)
+    .addTo(this.map);
+
+ }
+
+ zoomIn(){
+  this.map.zoomIn();
+ }
+
+ zoomOut(){
+  this.map.zoomOut();
+}
+
+  zoomChanges(value:string){
+    this.map.zoomTo(Number(value));
+  }
 }
--- src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -18,34 +18,61 @@ import * as mapboxgl from 'mapbox-gl';
       padding: 10px;
       border-radius: 5px;
       z-index: 999;
+      width: 400px;
     }
   `
   ]
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('map') divMap!: ElementRef; 
   map!:mapboxgl.Map; 
   zoomLevel: number = 17; 
+  mapCenter: [number, number] = [13.378491055638413, 52.51636916493755]
 
   constructor() { }
+
+  ngOnDestroy(): void {
+    this.map.off('zoom', ()=>{})
+    this.map.off('zoomend', ()=>{})
+    this.map.off('move', ()=>{})
+  }
 
   ngAfterViewInit(): void {
     this.map = new mapboxgl.Map({
     container: this.divMap.nativeElement,
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [13.378491055638413, 52.51636916493755],
+    center: this.mapCenter,
     zoom: this.zoomLevel
   });
+
+  this.map.on('zoom', ()=> {
+    this.zoomLevel=this.map.getZoom(); 
+  });
+
+  this.map.on('zoomend', ()=> {
+    if (this.map.getZoom()>18){
+      this.map.zoomTo(18);
+    }
+  })
+
+  this.map.on('move', (event)=> {
+    const target = event.target;
+    const {lng, lat}=target.getCenter(); 
+    this.mapCenter=[lng, lat];
+  });
+
  }
 
  zoomIn(){
   this.map.zoomIn();
-  this.zoomLevel=this.map.getZoom();
  }
 
  zoomOut(){
   this.map.zoomOut();
-  this.zoomLevel=this.map.getZoom();
 }
+
+  zoomChanges(value:string){
+    this.map.zoomTo(Number(value));
+  }
 }
